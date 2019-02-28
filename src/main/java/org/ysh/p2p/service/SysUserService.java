78bd7b64e925e233622ebb9ae5c3e79f7f45899e
yshin1992 <yshin1992@163.com>
2019-02-28 12:22:46 +0800

    增加用户验证方法

--- src/main/java/org/ysh/p2p/service/SysUserService.java
+++ src/main/java/org/ysh/p2p/service/SysUserService.java
@@ -4,6 +4,5 @@ import org.ysh.p2p.model.SysUser;
 
 public interface SysUserService extends AbstractService<SysUser> {
 	
-	
-	
+	public SysUser validateUser(String userCd , String passwd);
 }
--- src/main/java/org/ysh/p2p/service/impl/SysUserServiceImpl.java
+++ src/main/java/org/ysh/p2p/service/impl/SysUserServiceImpl.java
@@ -5,6 +5,7 @@ import org.ysh.p2p.dao.SysUserDao;
 import org.ysh.p2p.dao.impl.SysUserDaoImpl;
 import org.ysh.p2p.model.SysUser;
 import org.ysh.p2p.service.SysUserService;
+import org.ysh.p2p.util.StringUtil;
 
 public class SysUserServiceImpl extends AbstractServiceImpl<SysUser> implements
 		SysUserService {
@@ -13,6 +14,14 @@ public class SysUserServiceImpl extends AbstractServiceImpl<SysUser> implements
 	
 	public AbstractDao<SysUser> getDao() {
 		return sysUserDao;
+	}
+
+	public SysUser validateUser(String userCd, String passwd) {
+		SysUser user = new SysUser();
+		user.setUserCd(userCd);
+		user.setPassword(StringUtil.getMd5(StringUtil.getMd5(passwd) + userCd));
+		user.setIsAdmin(null);//屏蔽这个参数，因为用户在登录时不需要选择
+		return this.query(user, SysUser.class);
 	}
 
 }
