1c40082c916a6b54ec7a690da7f7ccdb51fd9eaf
yshin1992 <yshin1992@163.com>
2019-03-08 09:06:10 +0800

    增加普通会员登录选项

--- src/main/java/org/ysh/p2p/session/Session.java
+++ src/main/java/org/ysh/p2p/session/Session.java
@@ -1,5 +1,6 @@
 package org.ysh.p2p.session;
 
+import org.ysh.p2p.model.Member;
 import org.ysh.p2p.model.SysUser;
 
 public class Session {
@@ -7,6 +8,8 @@ public class Session {
 	private static final Session session = new Session();
 	
 	private SysUser loginUser;
+	
+	private Member loginMember;
 	
 	private Session(){
 	}
@@ -22,5 +25,14 @@ public class Session {
 	public void setLoginUser(SysUser loginUser) {
 		this.loginUser = loginUser;
 	}
+
+	public Member getLoginMember() {
+		return loginMember;
+	}
+
+	public void setLoginMember(Member loginMember) {
+		this.loginMember = loginMember;
+	}
+	
 	
 }
